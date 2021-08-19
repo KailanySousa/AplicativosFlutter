6e5cca083c152ea8f13c6b888560d704b4d0e468
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-18 22:18:27 -0300

    Lista de Tarefas: itens removíveis

--- ListaTarefas/app/lib/main.dart
+++ ListaTarefas/app/lib/main.dart
@@ -89,25 +89,42 @@ class _HomeState extends State<Home> {
             child: ListView.builder(
               padding: EdgeInsets.only(top: 10),
               itemCount: _toDoList.length,
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                  title: Text(_toDoList[index]["title"]),
-                  value: _toDoList[index]["ok"],
-                  secondary: CircleAvatar(
-                    child: Icon(
-                        _toDoList[index]["ok"] ? Icons.check : Icons.error),
-                  ),
-                  onChanged: (check) {
-                    setState(() {
-                      _toDoList[index]["ok"] = check;
-                      _saveData();
-                    });
-                  },
-                );
-              },
+              itemBuilder: _buildItem,
             ),
           )
         ],
+      ),
+    );
+  }
+
+  Widget _buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().microsecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (check) {
+          setState(
+            () {
+              _toDoList[index]["ok"] = check;
+              _saveData();
+            },
+          );
+        },
       ),
     );
   }
