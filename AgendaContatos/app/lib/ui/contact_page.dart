e0a10257844bd36ba2d4f0ed54fb75cd5c303911
KailanySousa <kailany-sousa2010@hotmail.com>
2021-09-01 12:45:27 -0300

    Agenda de Contatos: exibindo um AlertDialog

--- AgendaContatos/app/lib/ui/contact_page.dart
+++ AgendaContatos/app/lib/ui/contact_page.dart
@@ -34,74 +34,108 @@ class _ContactPageState extends State<ContactPage> {
     }
   }
 
+  Future<bool> _requestPop() {
+    if (_userEdited) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("Descartar Alterações?"),
+              content: Text("Se sair as alterações serão perdidas."),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Cancelar"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                  child: Text("Sim"),
+                ),
+              ],
+            );
+          });
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text(_editedContact.name ?? "Novo Contato"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
-            Navigator.pop(context, _editedContact);
-          } else {
-            FocusScope.of(context).requestFocus(_nameFocus);
-          }
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.red,
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            GestureDetector(
-              child: Container(
-                width: 140,
-                height: 140,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: _editedContact.img != null
-                          ? FileImage(File(_editedContact.img))
-                          : AssetImage("images/person.png") as ImageProvider),
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text(_editedContact.name ?? "Novo Contato"),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
+              Navigator.pop(context, _editedContact);
+            } else {
+              FocusScope.of(context).requestFocus(_nameFocus);
+            }
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.red,
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              GestureDetector(
+                child: Container(
+                  width: 140,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: _editedContact.img != null
+                            ? FileImage(File(_editedContact.img))
+                            : AssetImage("images/person.png") as ImageProvider),
+                  ),
                 ),
               ),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: "Nome"),
-              onChanged: (text) {
-                _userEdited = true;
+              TextField(
+                decoration: InputDecoration(labelText: "Nome"),
+                onChanged: (text) {
+                  _userEdited = true;
 
-                setState(() {
-                  _editedContact.name = text;
-                });
-              },
-              controller: _nameController,
-              focusNode: _nameFocus,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: "E-mail"),
-              onChanged: (text) {
-                _userEdited = true;
-                _editedContact.email = text;
-              },
-              keyboardType: TextInputType.emailAddress,
-              controller: _emailController,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: "Phone"),
-              onChanged: (text) {
-                _userEdited = true;
-                _editedContact.phone = text;
-              },
-              keyboardType: TextInputType.phone,
-              controller: _phoneController,
-            )
-          ],
+                  setState(() {
+                    _editedContact.name = text;
+                  });
+                },
+                controller: _nameController,
+                focusNode: _nameFocus,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "E-mail"),
+                onChanged: (text) {
+                  _userEdited = true;
+                  _editedContact.email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+                controller: _emailController,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Phone"),
+                onChanged: (text) {
+                  _userEdited = true;
+                  _editedContact.phone = text;
+                },
+                keyboardType: TextInputType.phone,
+                controller: _phoneController,
+              )
+            ],
+          ),
         ),
       ),
     );
