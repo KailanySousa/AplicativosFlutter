197e6ceb7c37c941720083f4336ddaf13faafb72
KailanySousa <kailany-sousa2010@hotmail.com>
2021-10-11 17:57:32 -0300

    fix

--- AgendaContatos/app/lib/helpers/contact_helper.dart
+++ AgendaContatos/app/lib/helpers/contact_helper.dart
@@ -38,13 +38,13 @@ class ContactHelper {
   }
 
   Future<Contact> saveContact(Contact contact) async {
-    Database dbContact = await _db;
+    Database dbContact = await db;
     contact.id = await dbContact.insert(contactTable, contact.toMap());
     return contact;
   }
 
   Future<Contact?> getContact(int id) async {
-    Database dbContact = await _db;
+    Database dbContact = await db;
     List<Map> maps = await dbContact.query(contactTable,
         columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
         where: "$idColumn = ?",
@@ -58,19 +58,20 @@ class ContactHelper {
   }
 
   Future<int> deleteContact(int id) async {
-    Database dbContact = await _db;
+    Database dbContact = await db;
     return await dbContact
         .delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
   }
 
   Future<int> updateContact(Contact contact) async {
-    Database dbContact = await _db;
+    Database dbContact = await db;
     return await dbContact.update(contactTable, contact.toMap(),
         where: "$idColumn = ?", whereArgs: [contact.id]);
   }
 
   Future<List> getAllContacts() async {
-    Database dbContact = await _db;
+    Database dbContact = await db;
+    print(dbContact);
     List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
 
     List<Contact> listContact = [];
@@ -82,13 +83,13 @@ class ContactHelper {
   }
 
   Future<int?> getNumber() async {
-    Database dbContact = await _db;
+    Database dbContact = await db;
     return Sqflite.firstIntValue(
         await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
   }
 
   Future close() async {
-    Database dbContact = await _db;
+    Database dbContact = await db;
     dbContact.close();
   }
 }
