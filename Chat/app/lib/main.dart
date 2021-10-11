39bf2d35ba81ab1594836f7085c20b5ceeb0201c
KailanySousa <kailany-sousa2010@hotmail.com>
2021-10-11 20:05:15 -0300

    criando campo de digitação

--- Chat/app/lib/main.dart
+++ Chat/app/lib/main.dart
@@ -1,20 +1,31 @@
+import 'package:app/chat_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
+
+  // await Firebase.initializeApp();
+
+  // QuerySnapshot snapshot =
+  //     await FirebaseFirestore.instance.collection('mensagens').get();
+
+  // snapshot.docs.forEach((d) {
+  //   print(d.data);
+  // });
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chat Flutter',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
+          primarySwatch: Colors.blue,
+          iconTheme: IconThemeData(color: Colors.blue)),
+      home: ChatScreen(),
     );
   }
 }
